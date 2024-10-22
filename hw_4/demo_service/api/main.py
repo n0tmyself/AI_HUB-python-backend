ca1e454cc9cc2fcadd467e745c5fee22fe68e39c
n0tmyself <alshevskiy.dima@mail.ru>
2024-10-22 17:41:41 +0300

    HW4

---
+++ hw_4/demo_service/api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from lecture_4.demo_service.api import users, utils
+
+
+def create_app():
+    app = FastAPI(
+        title="Testing Demo Service",
+        lifespan=utils.initialize,
+    )
+
+    app.add_exception_handler(ValueError, utils.value_error_handler)
+    app.include_router(users.router)
+
+    return app
+
+
+app = create_app()
+Instrumentator().instrument(app).expose(app)
